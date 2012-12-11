d9f24fb540bbc3e85ad8357b0f00e47ba1f681f7
Stoyan Stoyanov <djstoxer@gmail.com>
2012-12-11 16:42:20 +0200

    this is project containing some comment programing taskt. It would be updated some time :)

---
+++ SpiralMatrix/Program.cs
@@ -0,0 +1,154 @@
+﻿/*
+ * Generating spiral matrix.
+ */
+
+using System;
+
+namespace Problem14SpiralMatrix
+{
+    class Program
+    {
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("{0, 5}", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void FillSpiralMatrix(int[,] matrix)
+        {
+            int counter = 1;
+            int matrixSize = matrix.GetLength(0) * matrix.GetLength(1);
+            int row = 0, col = 0;
+            bool decCol = false, decRow = false;
+            int step = matrix.GetLength(0);
+            bool changeStep = true;
+            int it = 0;
+
+            while (true)
+            {
+                if (!decCol)
+                {
+                    while (it < step)
+                    {
+                        matrix[row, col] = counter;
+                        col++;
+                        counter++;
+                        it++;
+                    }
+                    col--;
+                    row++;
+                    it = 0;
+                    decCol = true;
+                    if (changeStep)
+                    {
+                        changeStep = false;
+                        step -= 1;
+                    }
+                    else
+                        changeStep = true;
+                    if (counter == matrixSize)
+                    {
+                        matrix[row, col] = counter;
+                        return;
+                    }
+                }
+                if (!decRow)
+                {
+                    while (it < step)
+                    {
+                        matrix[row, col] = counter;
+                        row++;
+                        counter++;
+                        it++;
+                    }
+                    row--;
+                    col--;
+                    it = 0;
+                    decRow = true;
+                    if (changeStep)
+                    {
+                        changeStep = false;
+                        step -= 1;
+                    }
+                    else
+                        changeStep = true;
+                    if (counter == matrixSize)
+                    {
+                        matrix[row, col] = counter;
+                        return;
+                    }
+                }
+
+                if (decCol)
+                {
+                    while (it < step)
+                    {
+                        matrix[row, col] = counter;
+                        col--;
+                        counter++;
+                        it++;
+                    }
+                    col++;
+                    row--;
+                    it = 0;
+                    decCol = false;
+                    if (changeStep)
+                    {
+                        changeStep = false;
+                        step -= 1;
+                    }
+                    else
+                        changeStep = true;
+                    if (counter == matrixSize)
+                    {
+                        matrix[row, col] = counter;
+                        return;
+                    }
+                }
+
+                if (decRow)
+                {
+                    while (it < step)
+                    {
+                        matrix[row, col] = counter;
+                        row--;
+                        counter++;
+                        it++;
+                    }
+                    row++;
+                    col++;
+                    it = 0;
+                    decRow = false;
+                    if (changeStep)
+                    {
+                        changeStep = false;
+                        step -= 1;
+                    }
+                    else
+                        changeStep = true;
+                    if (counter == matrixSize)
+                    {
+                        matrix[row, col] = counter;
+                        return;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n = ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            FillSpiralMatrix(matrix);
+            PrintMatrix(matrix);
+        }
+    }
+}
---
+++ TrailingZeros/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Trailing zero of number!
+*/
+
+using System;
+
+namespace Problem13TrailingZeroes
+{
+    class Program
+    {
+        static int GetTrailingZeroes(int n)
+        {
+            int counter = 0;
+
+            while (n != 0)
+            {
+                n /= 5;
+                counter += n;
+            }
+
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The trailing zeroes of {0}! is {1}", n, GetTrailingZeroes(n));
+        }
+    }
+}
